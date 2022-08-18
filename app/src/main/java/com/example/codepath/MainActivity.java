745dca7c6353a97ed122ae8fc524b2656b641ca7
Favour Salako <salakofavour0@gmail.com>
2022-08-17 22:16:33 -0400

    CodePath prework creating the design and functionality

---
+++ app/src/main/java/com/example/codepath/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.codepath;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button hi = findViewById(R.id.hello);
+
+
+        hi.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(MainActivity.this, "Hello to you too", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+    }
+}
